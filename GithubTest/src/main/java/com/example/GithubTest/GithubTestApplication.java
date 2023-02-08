3a7a7a2e287748797df79e1485c24ccc7cdf6800
JensonSumHKET <jensonsum@hket.com>
2023-02-08 12:24:45 +0800

    Update GithubTestApplication.java

--- GithubTest/src/main/java/com/example/GithubTest/GithubTestApplication.java
+++ GithubTest/src/main/java/com/example/GithubTest/GithubTestApplication.java
@@ -8,6 +8,7 @@ public class GithubTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubTestApplication.class, args);
+		System.out.println("hello");
 	}
 
 }
